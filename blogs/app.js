9be006e805e5e8845e0e9117ddf4cee02985f259
unknown <yadavharsh400@gmail.com>
2018-09-29 00:50:58 -0700

    add authentication

--- blogs/app.js
+++ blogs/app.js
@@ -1,11 +1,39 @@
 var app = require("express")();
 var mongoose = require("mongoose");
 var bodyparser = require("body-parser");
+var User = require("./models/user");
+var passport = require("passport");
+var local = require("passport-local");
+var plm = require("passport-local-mongoose");
 app.set("view engine","ejs");
 var methodOverride = require("method-override");
+
+//====using the required packages ! =====
 app.use(bodyparser.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+
+app.use(require("express-session")({
+  secret : "this is the secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new local(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+  res.locals.currentUser = req.user;
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/blogsite');
+
 
 var blogschema = new mongoose.Schema({
    title: String,
@@ -29,7 +57,7 @@ app.get("/",function(req,res){
     //res.render("landing",{blogs:blgs});
 });
 
-app.get("/blogs/create",function(req,res){
+app.get("/blogs/create",isloggedin,function(req,res){
    res.render("new"); 
 });
 
@@ -57,7 +85,7 @@ app.get("/blogs/:id",function(req,res){
      
 });
 
-app.get("/blogs/:id/addcomment",function(req,res){
+app.get("/blogs/:id/addcomment",isloggedin,function(req,res){
 	Blog.findById(req.params.id,function(err,user){
 		if(err) console.log(err);
 		else
@@ -73,7 +101,7 @@ app.post("/blogs/:id",function(req,res){
 		if(err) console.log(err);
 		else
 		{
-			blog.comments.push({author:req.body.Author,content:req.body.Content});
+			blog.comments.push({author:req.user.username,content:req.body.Content});
 			blog.save();
 			//res.redirect("/blogs/"+req.params.id);
 			console.log(blog.comments);
@@ -90,6 +118,51 @@ app.post("/blogs",function(req,res){
         //else console.log(blog);
     });
     res.redirect("/");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+    if(err)
+    {
+      console.log(err);
+      res.redirect("/register");
+    }
+    else
+    {
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/");
+      });
+    }
+  });
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.post("/login",passport.authenticate("local",{successRedirect:"/",failureRedirect:"/login"}),
+  function(req,res){
+});
+
+function isloggedin(req,res,next){
+  if(req.isAuthenticated())
+  {
+    return next();
+  }
+  res.redirect("/login")
+}
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.get("/secret",isloggedin,function(req,res){
+  res.render("secret");
 });
 
 app.listen(process.env.PORT||3000,process.env.IP,function(){
---
+++ blogs/models/user.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var plm = require("passport-local-mongoose");
+var passport = require("passport");
+
+var userSchema = new mongoose.Schema({
+	username : String,
+	password : String
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("User",userSchema);
